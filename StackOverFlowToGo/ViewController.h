743e8c405ca44cd0bcc2e26577f232d5da794a99
Reid Weber <reid_weber@hotmail.com>
2014-11-10 13:21:12 -0800

    Initial Commit

---
+++ StackOverFlowToGo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverFlowToGo
+//
+//  Created by Reid Weber on 11/10/14.
+//  Copyright (c) 2014 Reid Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
